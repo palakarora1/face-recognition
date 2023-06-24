942b6c922f0011a937ae11214d6837ca552676d4
Palak Arora <107752408+palakarora1@users.noreply.github.com>
2023-06-24 21:31:56 +0530

    data.js

--- frb/src/component/data.js
+++ frb/src/component/data.js
@@ -13,33 +13,7 @@ const Data = () => {
         )
     },[])
     return (
-        <div>
-            <div>
-                <h1 style={{textAlign: "center"}}> Face Recognition Based Attendance System</h1>
-            </div>
-            <table class="center">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Date</th>
-                        <th>Time</th>
-                        <th>Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        item.map(item => (
-                            <tr key={item.id}>
-                                <td>{item.name}</td>
-                                <td>{item.date}</td>
-                                <td>{item.time}</td>
-                                <td>{item.status}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
-        </div>
+       
     )
 }
 
